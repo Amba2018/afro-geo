ef30093a4babbbf301bdfebf6ad5c463b1ea4ef3
Emmanuel Manu <manu.emma.em@gmail.com>
2024-03-04 00:01:51 +0000

    Added structured questions in a new js file

---
+++ assets/js/questions.js
@@ -0,0 +1,111 @@
+const questions = [
+    {
+        question: "What is the Capital City of Ghana?",
+        answers: [
+            { text: "Accra", correct: true },
+            { text: "Kiev", correct: false },
+            { text: "Abu Dhabi", correct: false },
+            { text: "Hanoi", correct: false }
+        ]
+    },
+    {
+        question: "What is the Capital City of Egypt?",
+        answers: [
+            { text: "Cairo", correct: true },
+            { text: "Sanaa", correct: false },
+            { text: "Harare", correct: false },
+            { text: "Bamako", correct: false }
+        ]
+    },
+    {
+        question: "What is the Capital City of Germany?",
+        answers: [
+            { text: "London", correct: false },
+            { text: "New Delhi", correct: false },
+            { text: "Berlin", correct: true },
+            { text: "Amsterdam", correct: false }
+        ]
+    },
+    {
+        question: "What is the Capital City of Greece?",
+        answers: [
+            { text: "Tripoli", correct: false },
+            { text: "Uruguay", correct: false },
+            { text: "Athens", correct: true },
+            { text: "Oslo", correct: false }
+        ]
+    },
+    {
+        question: "What is the Capital City of India?",
+        answers: [
+            { text: "Abu Dhabi", correct: false },
+            { text: "Kampala", correct: false },
+            { text: "New Delhi", correct: true },
+            { text: "Oslo", correct: false }
+        ]
+    },
+    {
+        question: "What is the Capital City of Japan?",
+        answers: [
+            { text: "Harare", correct: false },
+            { text: "Bamako", correct: false },
+            { text: "Tokyo", correct: true },
+            { text: "Kampala", correct: false }
+        ]
+    },
+    {
+        question: "Which country has Tripoli as capital?",
+        answers: [
+            { text: "France", correct: false },
+            { text: "Libya", correct: true },
+            { text: "Mali", correct: false },
+            { text: "Yemen", correct: false }
+        ]
+    },
+    {
+        question: "Which country has Amsterdam as capital?",
+        answers: [
+            { text: "Ukraine", correct: false },
+            { text: "New Zealand", correct: false },
+            { text: "Japan", correct: false },
+            { text: "Netherlands", correct: true }
+        ]
+    },
+    {
+        question: "Which country has Oslo as capital?",
+        answers: [
+            { text: "Yemen", correct: false },
+            { text: "Uganda", correct: false },
+            { text: "Norway", correct: true },
+            { text: "Greece", correct: false }
+        ]
+    },
+    {
+        question: "Which country has London as capital?",
+        answers: [
+            { text: "Vietnam", correct: false },
+            { text: "Vatican City", correct: false },
+            { text: "Vanuatu", correct: false },
+            { text: "United Kingdom", correct: true }
+        ]
+    },
+    {
+        question: "Which country has Kampala as capital?",
+        answers: [
+            { text: "Vanuatu", correct: false },
+            { text: "Venezuela", correct: false },
+            { text: "Yemen", correct: false },
+            { text: "Uganda", correct: true }
+        ]
+    },
+    {
+        question: "Which country has Port-Vila as capital?",
+        answers: [
+            { text: "Ukraine", correct: false },
+            { text: "Japan", correct: false },
+            { text: "Vanuatu", correct: true },
+            { text: "Uzbekistan", correct: false }
+        ]
+    },
+   
+];
